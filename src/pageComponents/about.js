532fa8f61b6c169dee169736031f3eefb3505039
randy steward <rsteward117@gmail.com>
2022-06-30 21:17:09 -0500

    finished project

--- src/pageComponents/about.js
+++ src/pageComponents/about.js
@@ -19,16 +19,13 @@ function About(){
                         <img src={myPortrait} alt="A picture of myself" />
                     </section>
                     <section className="my-story grid-cell">
-                        <div className="my-story-header">
-                            <h1>About Myself</h1>
-                        </div>
                         <p>
                             I've been self teaching myself to code and program, In hopes to gain employment as an frontend developer. 
                             With this portfolio site I will demonstrate my skills and knowledge I've gathered so far on my self taught journey.
                         </p>
                     </section>
                     <section className="my-skills grid-cell">
-                        <div className="my-skills-header"><h2>My Skills</h2></div>
+                        <div className="my-skills-header"><h1>My Skills</h1></div>
                         <div className="skills-img-grid">
                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg" />
                             <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" />
